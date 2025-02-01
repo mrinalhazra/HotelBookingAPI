61c5b21323b16f0402e666928362cb704d7f7402
Mrinal <mrinalhazra28@gmail.com>
2025-02-01 15:34:35 +0530

    Add spring security remember me cookie code

--- src/main/java/com/basicApi/hotel/config/SecurityConfig.java
+++ src/main/java/com/basicApi/hotel/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.basicApi.hotel.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,16 +17,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/user/register")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                .antMatchers("/user/register").permitAll()
                 .and()
-                .httpBasic();
+                .rememberMe().userDetailsService(userDetailsService)
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().deleteCookies("rememberMe");
+
        return http.build();
     }
 
